dbb5b2c20c960f0fc03c8ef7f8fc3ad48b092fe2
pcthomart <pcthomart@yahoo.fr>
2019-10-01 11:50:53 +0200

    [CLEAN] Remove fmod and xiamo from shared

--- Script/Lib/Tools/FmodAnimationHelper.cs
+++ Script/Lib/Tools/FmodAnimationHelper.cs
@@ -6,11 +6,14 @@ public class FmodAnimationHelper : MonoBehaviour {
 
 	public void PlaySound( string sSoundName )
     {
-        FMODUnity.RuntimeManager.PlayOneShot(sSoundName); // "event:/GoodMove");
+        Debug.LogError("FMOD is no more in shared, please copy this script in your project");
+        //FMODUnity.RuntimeManager.PlayOneShot(sSoundName); // "event:/GoodMove");
     }
 
     public void PlaySoundWithFloatParameter(string sSoundName, string sParamName, float fValue )
     {
+        Debug.LogError("FMOD is no more in shared, please copy this script in your project");
+/*
         FMOD.Studio.EventInstance eventInstance = FMODUnity.RuntimeManager.CreateInstance(sSoundName);
         FMOD.Studio.ParameterInstance paramInstance;
         if ( eventInstance.getParameter(sParamName, out paramInstance) != FMOD.RESULT.OK )
@@ -20,6 +23,6 @@ public class FmodAnimationHelper : MonoBehaviour {
         }
         paramInstance.setValue(fValue);
         eventInstance.start();
-        eventInstance.release();
+        eventInstance.release();*/
     }
 }
